20e0905ffb17529b477d6c34d1e63487e681a930
Rajat Mourya <rm24042000@gmail.com>
2024-06-09 00:44:10 +0530

    Factorial

---
+++ lacture6/Factorial.java
@@ -0,0 +1,19 @@
+package lacture6;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+//    Write a java program to input a number and calculate its factorial using for loop.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the Number: ");
+        int n = sc.nextInt();
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+    }
+
+}
