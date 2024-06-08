058fb1bda69fdf7483716dadb6f3b924f4614830
Rajat Mourya <rm24042000@gmail.com>
2024-06-08 15:05:06 +0530

    Miscellaneous Star Pattern

--- lacture17ArrayList/MergeSortedArray.java
+++ lacture17ArrayList/MergeSortedArray.java
@@ -1,4 +1,25 @@
 package lacture17ArrayList;
 
+import java.util.Arrays;
+
 public class MergeSortedArray {
+    public static void main(String[] args) {
+        int nums1[] = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        merge(nums1,m,nums2,n);
+        System.out.print("[ ");
+        for(int e: nums1){
+            System.out.print(e+" ");
+        }
+        System.out.print("]");
+
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
 }
--- lacture4/MiscellaneousStarPattern.java
+++ lacture4/MiscellaneousStarPattern.java
@@ -6,13 +6,18 @@ public class MiscellaneousStarPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number of Rows: ");
-//        int n = sc.nextInt();
-        int n = 3;
-//        diamondStarPattern(n);
-//        rightArrowStarPattern(n);
-//        plusSignPattern(n);
-//        crossStarPattern(n);
+        int n = sc.nextInt();
+//        int n = 5;
+        diamondStarPattern(n);
+        System.out.println();
+        rightArrowStarPattern(n);
+        System.out.println();
+        plusSignPattern(n);
+        System.out.println();
+        crossStarPattern(n);
+        System.out.println();
         eightStarPattern(n);
+        System.out.println();
 
     }
 
@@ -110,5 +115,25 @@ public class MiscellaneousStarPattern {
 
     public static void eightStarPattern(int n) {
 
+        for (int i = 1; i <= n*2-1; i++) {
+            if(i == 1 || i == n || i == n*2 -1){
+                for (int j = 1; j < n; j++) {
+                    if(j > 1 && j < n) {
+                        System.out.print("*");
+                    }else {
+                        System.out.print(" ");
+                    }
+                }
+            }else {
+                for (int j = 1; j <= n; j++) {
+                    if(j == 1 || j == n){
+                        System.out.print("*");
+                    }else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
