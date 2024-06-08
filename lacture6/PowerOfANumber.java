d7aa764ef7fa8783548f9e7981921863193323b7
Rajat Mourya <rm24042000@gmail.com>
2024-06-09 00:35:44 +0530

    Power Of A Number

---
+++ lacture6/PowerOfANumber.java
@@ -0,0 +1,20 @@
+package lacture6;
+
+import java.util.Scanner;
+
+public class PowerOfANumber {
+
+//    Write a java program to find power of a number using for loop.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the Base: ");
+        int base = sc.nextInt();
+        System.out.println("enter the Exponent: ");
+        int exponent = sc.nextInt();
+        int result = 1;
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+        System.out.println(result);
+    }
+}
